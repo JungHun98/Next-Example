e0cdade838f82f436407822c0c3e06901a0ea26d
김정훈 <j7papa@naver.com>
2024-04-30 17:50:48 +0900

    feat: 데이터 조회 및 에러, 로딩 페이지 연습

--- weather-app/src/app/[location]/page.tsx
+++ weather-app/src/app/[location]/page.tsx
@@ -1,3 +1,4 @@
+import { getForcecast } from "@/utils/getForcecast";
 import HomeButton from "../components/HomeButton";
 
 type Props = {
@@ -6,12 +7,20 @@ type Props = {
   };
 };
 
-export default function Detail({ params }: Props) {
+export default async function Detail({ params }: Props) {
   const name = params.location === "seoul" ? "서울" : "";
+  const res = await getForcecast(params.location);
 
   return (
     <>
       <h1>{name}의 3일 예고</h1>
+      <ul>
+        {res.forecast.forecastday.map((day) => (
+          <li key={day.date}>
+            {day.date}/{day.day.avgtemp_c}
+          </li>
+        ))}
+      </ul>
       <HomeButton />
     </>
   );
--- weather-app/src/app/page.tsx
+++ weather-app/src/app/page.tsx
@@ -1,13 +1,18 @@
 import Link from "next/link";
 import style from "./style.module.css";
+import { getCurrentweather } from "@/utils/getCurrentweather";
 
-export default function Home() {
+export default async function Home() {
+  const res = await getCurrentweather("seoul");
+  // console.log(res.current.condition.text);
+
   return (
     <>
       <h1>main</h1>
       <ul className={style.list}>
         <li>
           <Link href="/seoul">서울</Link>
+          <span>{res.current.condition.text}</span>
         </li>
         <li>
           <Link href="/newyork">뉴욕</Link>
